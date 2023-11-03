30e9e61071fb2ad3836109ebc45aaa0becb575bd
Dustin Smith <dusmith0@tamu.edu>
2023-11-03 18:06:36 -0500

    I added some pieces to the birthday simulation.

--- R/sim.R
+++ R/sim.R
@@ -17,14 +17,14 @@ sims <- list("Birthday","Monty.Hall.Auto","Monty.Hall","Sampling","Law.of.Large.
 Birthday_fun <- function(Iter = 50,size = NULL,plot=TRUE){
   #Figure out how to make the size allow many inputs with c.
   if(is.null(size)){
-    size <- c(10,20,30,40,50,75,100)
+    size <- c(10,20,30,40)
   }
 
   prob <- as.data.frame(matrix(ncol=length(size),nrow=(Iter)))
   test <- prob
   names(prob) <- as.character(size)
 
-  par(mfrow=c(3,3))
+  par(mfrow=c(2,2),mtext("Birthday Simulation",side=3,adj=.5))
   for(j in seq_along(size)){
     for(i in 1:Iter){
       population <- sample(seq(1:365),size[j],replace = TRUE)
@@ -33,7 +33,9 @@ Birthday_fun <- function(Iter = 50,size = NULL,plot=TRUE){
 
     }
     if(plot == TRUE){
-      plot(prob[,j])
+      plot(prob[,j],main=paste("Size",size[j]),xlab="interations",ylab="prob",xlim=c(1,Iter),
+      ylim=seq(0,1),col=palette()[2])
+      abline(h = mean(prob[,j]))
     }
   }
 
