12171b880b5745a7a7cd915fa3d15fa1411d95e1
Dustin Smith <dusmith0@tamu.edu>
2023-11-02 23:49:59 -0500

    I built a simple function to pull data files with just the data file name. I need to add some catchs for different data types, an error code that prints a list of data sets if desired. Also would like to add the ability to extract certain data from the files.

--- R/get_data.R
+++ R/get_data.R
@@ -6,6 +6,19 @@
 #' @export
 #'
 #' @examples
-get_data <- function(data_name){
+get_data <- function(data_name,ap_data_set = TRUE){
+  if(ap_data_set == TRUE){
+    data_file <- readline()
+  }
 
+  if(){#input string check for last three digits of the script. i.e. txt
+  data.set <- try(read.table(paste("apdata",data_file,sep="/"),header = F))
+  }
+  else(){#input string check for last three digits of the script. i.e. csv
+  data.set <- try(read.csv(paste("apdata",data_file,sep="/"),header = F))
+  }
+  ## TO DO create another else for code that inputs EXCELL files.
 }
+
+
+data_list <- list("Moneyball","Movies")
