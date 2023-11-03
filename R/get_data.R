a986ac7b619c2185b5b8554bb6543af246d59757
Dustin Smith <dusmith0@tamu.edu>
2023-11-03 00:03:16 -0500

    I added more comments of what to include in get_data function.

--- R/get_data.R
+++ R/get_data.R
@@ -18,6 +18,16 @@ get_data <- function(data_name,ap_data_set = TRUE){
   data.set <- try(read.csv(paste("apdata",data_file,sep="/"),header = F))
   }
   ## TO DO create another else for code that inputs EXCELL files.
+
+  ##Include a set of code that allows input from BASE data sets, as well as other packages/
+  ##This might be over my head data(package = .packages(all.available = TRUE))
+
+  ##TO DO error code that can in desired print a list of data set names to type in.
+
+  ##TO DO create a fucntion that allows a user to select specific peices of data from each file.
+  ## such as 100 data peices, or 10, or specific feilds of data. Also allow print of data to be viewed.
+
+  ##Create a function that can print a pdf of the data set if deired.
 }
 
 
