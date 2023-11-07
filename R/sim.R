f0ae2e73924b47f98c36205ba2fff31f120c7bf9
Dustin Smith <dusmith0@tamu.edu>
2023-11-06 20:48:27 -0600

    I added some beatification to the birthday simulation.

--- R/sim.R
+++ R/sim.R
@@ -27,7 +27,7 @@ Birthday_fun <- function(Iter = 50,size = NULL,plot=TRUE){
   test <- prob
   names(prob) <- as.character(size)
 
-  par(mfrow=c(2,2),mtext("Birthday Simulation",side=3,adj=.5))
+  par(mfrow=c(2,2),bg="wheat")
   for(j in seq_along(size)){
     for(i in 1:Iter){
       population <- sample(seq(1:365),size[j],replace = TRUE)
@@ -37,8 +37,8 @@ Birthday_fun <- function(Iter = 50,size = NULL,plot=TRUE){
     }
     if(plot == TRUE){
       plot(prob[,j],main=paste("Size",size[j]),xlab="interations",ylab="prob",xlim=c(1,Iter),
-      ylim=seq(0,1),col=palette()[2])
-      abline(h = mean(prob[,j]))
+      ylim=seq(0,1),col=palette()[3],pch=16)
+      abline(h = mean(prob[,j]),col = "purple",lwd=2)
     }
   }
 
