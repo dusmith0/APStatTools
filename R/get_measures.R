61ed57eecc4cc5a27e34bf4dd8f24de684cfc3e6
Dustin Smith <dusmith0@tamu.edu>
2023-11-21 13:21:51 -0600

    I changed my get_measures function to output as a data.frame. It looks much better now.

--- R/get_measures.R
+++ R/get_measures.R
@@ -1,5 +1,5 @@
 ## A set of functions to find general values from a list or lists of data.
-get_measures <- function(input = FALSE, X = NULL, Y = NULL, freq = c(1), frequency = FALSE, regression = FALSE, outliers = NULL){
+get_measures <- function(input = FALSE, X = NULL, Y = NULL, freq = c(1), frequency = FALSE, regression = FALSE, outliers = FALSE){
   ## A piece to allow for user input for data, and reading it into different columns
   if(input == TRUE){
     data <- data.frame()
@@ -20,22 +20,28 @@ get_measures <- function(input = FALSE, X = NULL, Y = NULL, freq = c(1), frequen
   IQR <- rep(0,ncol(data))
   lower_outliers <- rep(0,ncol(data))
   upper_outliers <- rep(0,ncol(data))
+  measures <- data.frame(colnames(data))
 
   ## A peice to calculate all types of measures
     ## List(n, pop_mean, sample_mean, pop_sd, sample_sd, , Q1, Q2, Q3, IQR, r, r-squared, a, b for LSRL)
-      totals <- colSums(data)
-      n <- nrow(data)
-      mean <- colMeans(data)
-      pop_sd <- apply(data,2,function(z) sd(z))
-      sample_sd <- apply(data,2,function(z) sd(z))
+      measures[,2] <- totals <- colSums(data)
+      measures[,3] <- n <- nrow(data)
+      measures[,4] <- mean <- colMeans(data)
+      measures[,5] <- pop_sd <- apply(data,2,function(z) sd(z))
+      measures[,6] <- sample_sd <- apply(data,2,function(z) sd(z))
       for(i in 1:ncol(data)){
         Q1[i] <- mean(data[floor((n + 1) / 4),i], data[ceiling((n + 1) / 4),i])
         Q2[i] <- mean(data[floor((n + 1) / 2),i], data[ceiling((n + 1) / 2),i])
         Q3[i] <- mean(data[floor(3 * (n + 1) / 4),i], data[ceiling(3 * (n + 1) / 4),i])
       }
-      IQR <- Q3 - Q1
+      measures[,7] <- Q1
+      measures[,8] <- Q2
+      measures[,9] <- Q3
+      measures[,10] <- IQR <- Q3 - Q1
 
-      if(!is.null(outliers)){
+      names(measures) <- c("data","totals","n","mean","pop_sd","sample_sd","Q1","Q2","Q3","IQR")
+
+      if(outliers != FALSE){
         if(outliers == "1.5 IQR"){
           lower_outliers <- Q1 - 1.5 * IQR
           upper_outliers <- Q3 + 1.5 * IQR
@@ -43,25 +49,22 @@ get_measures <- function(input = FALSE, X = NULL, Y = NULL, freq = c(1), frequen
           lower_outliers <- mean - (3 * sample_sd)
           upper_outliers <- mean - (3 * sample_sd)
         }
+        measures[,11] <- lower_outliers
+        measures[,12] <- upper_outliers
+        names(measures)[11] <- "lower_outliers"
+        names(measures)[12] <- "upper_outliers"
       }
+
 
       if(regression == TRUE){
         LSRL <- summary(lm(data[,2]~data[,1]))
       }
 
-      if(regression == FALSE & outliers == FALSE){
-        return(list(data = data, totals = totals, n = n, mean = mean, pop_sd = pop_sd, sample_sd = sample_sd, Q1 = Q1, Q2 = Q2, Q3 = Q3, IQR = IQR))
-      }
-
-      if(regression == TRUE & outliers == FALSE){
-        return(list(data = data, totals = totals, n = n, mean = mean, pop_sd = pop_sd, sample_sd = sample_sd, Q1 = Q1, Q2 = Q2, Q3 = Q3, IQR = IQR, LSRL = LSRL))
-      }
-
-      if(regression == FALSE & outliers == TRUE){
-        return(list(data = data, totals = totals, n = n, mean = mean, pop_sd = pop_sd, sample_sd = sample_sd, Q1 = Q1, Q2 = Q2, Q3 = Q3, IQR = IQR, lower_outliers = lower_outliers, upper_outliers = upper_outliers))
+      if(regression == FALSE){
+        return(list(data = data, measures = measures))
       }
 
       if(regression == TRUE & outliers == TRUE){
-        return(list(data = data, totals = totals, n = n, mean = mean, pop_sd = pop_sd, sample_sd = sample_sd, Q1 = Q1, Q2 = Q2, Q3 = Q3, IQR = IQR, lower_outliers = lower_outliers, upper_outliers = upper_outliers, LSRL = LSRL))
+        return(list(data = data, measures = measures, LSRL = LSRL))
       }
 }
