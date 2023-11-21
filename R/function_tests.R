b757c102b91bac594604cf7a7f401ebfa2256ff8
Dustin Smith <dusmith0@tamu.edu>
2023-11-20 22:39:33 -0600

    I added several tests for the test() file. They each seem to work.

--- R/function_tests.R
+++ R/function_tests.R
@@ -29,4 +29,18 @@ tail = "left"
 z.test.pooled(p_hat,n,tail="left")
 
 
+## Chi-Squared GOF test
+null_table <- blood <- c(465,394,96,45)
+expected_table <- expected <- c(.45,.40,.1,.05)
+
+chi_squared_gof(null_table, expected_table)
+
+
+## Paired T-test.
+table_one <- males <- c(1902,1470,382,778,423,568,1375,682)
+table_two <- females <- c(221,633,200,312,629,435,2098,283)
+
+t_test_paired(null = 0,table_one,table_two,tail="two")
+
+
 
