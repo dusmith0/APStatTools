f10d4e64c043475e1b1c3078f2d5221f6f12c949
Dustin Smith <dusmith0@tamu.edu>
2023-12-07 10:02:56 -0600

    I changed sd to sigma on some more files.

--- tests/testthat/test_find_probs.R
+++ tests/testthat/test_find_probs.R
@@ -1,7 +1,7 @@
 test_that("That the find_probs function is producing the correct values",{
 
   expect_equal(find_probs(1.5),pnorm(1.5,0,1))
-  expect_equal(find_probs(1.5,mean = 12, sd = 5),pnorm(1.5,12,5))
+  expect_equal(find_probs(1.5,mean = 12, sigma = 5),pnorm(1.5,12,5))
   expect_equal(find_probs(c(-3,-2,-1,0,1,2,3)), c(pnorm(-3,0,1),pnorm(-2,0,1),pnorm(-1,0,1),
                 pnorm(0,0,1),pnorm(1,0,1),pnorm(2,0,1),pnorm(3,0,1)))
   expect_equal(find_probs(c(-1,3),type="chi-squared",tail="inner",df=4),
@@ -17,27 +17,27 @@ test_that("That the find_probs function is producing the correct values",{
   expect_equal(find_probs(4,type="binomial",tail="left_not_equal"),
                (pbinom(3, 10, .5)))
   expect_warning(find_probs(bound = .4, inverse = TRUE))
-  expect_warning(find_probs(bound = .8, mean = 10, sd = 4, inverse = TRUE))
+  expect_warning(find_probs(bound = .8, mean = 10, sigma = 4, inverse = TRUE))
 
 })
 
 test_that("That the find_probs produces the correct errors",{
   expect_warning(find_probs(bound = .4, inverse = TRUE))
-  expect_warning(find_probs(bound = .8, mean = 10, sd = 4, inverse = TRUE))
+  expect_warning(find_probs(bound = .8, mean = 10, sigma = 4, inverse = TRUE))
   expect_error(find_probs())
   expect_error(find_probs(2,"inner"))
   expect_error(find_probs(2,"two"))
   expect_error(find_probs(2,"outer"))
 
   expect_warning(find_probs(bound = .4, type = "t-dist", inverse = TRUE))
-  expect_warning(find_probs(bound = .8, type = "t-dist", mean = 10, sd = 4, inverse = TRUE))
+  expect_warning(find_probs(bound = .8, type = "t-dist", mean = 10, sigma = 4, inverse = TRUE))
   expect_error(find_probs(type = "t-dist"))
   expect_error(find_probs(2,type = "t-dist","inner"))
   expect_error(find_probs(2,type = "t-dist","two"))
   expect_error(find_probs(2,type = "t-dist","outer"))
 
   expect_warning(find_probs(bound = .4, type = "binomial", inverse = TRUE))
-  expect_warning(find_probs(bound = .8, type = "binomial", mean = 10, sd = 4, inverse = TRUE))
+  expect_warning(find_probs(bound = .8, type = "binomial", mean = 10, sigma = 4, inverse = TRUE))
   expect_error(find_probs(type = "binomial"))
   expect_error(find_probs(2,type = "binomial","inner"))
   expect_error(find_probs(2,type = "binomial","two"))
--- tests/testthat/test_statistical_test.R
+++ tests/testthat/test_statistical_test.R
@@ -1,48 +1,48 @@
 test_that("test preforms the one sample t-test correclty", {
-  found <- test(test = "t_test.one", null = 10, x_bar = 8, sd = 3, n = 25, tail = "left", graph = FALSE)
+  found <- test(test = "t_test.one", null = 10, x_bar = 8, sigma = 3, n = 25, tail = "left", graph = FALSE)
   null <- 10
   x_bar <- 8
-  sd <- 3
+  sigma <- 3
   n <- 25
 
   expect_equal(found$n, 25)
-  expect_equal(found$standard_error, sd/sqrt(n))
+  expect_equal(found$standard_error, sigma/sqrt(n))
   expect_equal(found$df, n - 1)
   expect_equal(found$test_statistic, (8 - 10)/(3/sqrt(25)))
   expect_equal(found$p_value, pt((8 - 10)/(3/sqrt(25)),25-1))
 
   #Testing for right tails
-  found <- test(test = "t_test.one", null = 10, x_bar = 8, sd = 3, n = 25, tail = "right", graph = FALSE)
+  found <- test(test = "t_test.one", null = 10, x_bar = 8, sigma = 3, n = 25, tail = "right", graph = FALSE)
   expect_equal(found$p_value, pt((8 - 10)/(3/sqrt(25)),25-1, lower.tail = FALSE))
 
   #testing for two tails
-  found <- test(test = "t_test.one", null = 10, x_bar = 8, sd = 3, n = 25, tail = "two", graph = FALSE)
+  found <- test(test = "t_test.one", null = 10, x_bar = 8, sigma = 3, n = 25, tail = "two", graph = FALSE)
   expect_equal(found$p_value, pt((8 - 10)/(3/sqrt(25)),25-1) * 2)
 
 })
 
 
 test_that("test preforms the two sample t-test correclty", {
-  found <- test(test = "t_test.two", null = 0, x_bar = c(12,9), sd = c(2,3), n = c(25,30), tail = "left", graph = FALSE)
+  found <- test(test = "t_test.two", null = 0, x_bar = c(12,9), sigma = c(2,3), n = c(25,30), tail = "left", graph = FALSE)
   null <- 0
   x_bar <- c(12,9)
-  sd <- c(2,3)
+  sigma <- c(2,3)
   n <- c(25,30)
   diff(-x_bar)
 
   expect_equal(found$n, c(25,30))
-  expect_equal(found$standard_error, c(sd[1]/sqrt(n[1]), sd[2]/sqrt(n[2])))
+  expect_equal(found$standard_error, c(sigma[1]/sqrt(n[1]), sigma[2]/sqrt(n[2])))
   expect_equal(round(found$df,3), round(c(50.742,50.742),3))
-  expect_equal(found$test_statistic[1], (12 - 9)/(sqrt( sd[1] ^ 2 / n[1] + sd[2] ^ 2 / n[2])))
-  expect_equal(found$p_value[1], pt((12 - 9)/(sqrt( sd[1] ^ 2 / n[1] + sd[2] ^ 2 / n[2])),50.742))
+  expect_equal(found$test_statistic[1], (12 - 9)/(sqrt( sigma[1] ^ 2 / n[1] + sigma[2] ^ 2 / n[2])))
+  expect_equal(found$p_value[1], pt((12 - 9)/(sqrt( sigma[1] ^ 2 / n[1] + sigma[2] ^ 2 / n[2])),50.742))
 
   #Testing for right tails
-  found <- test(test = "t_test.two", null = 0, x_bar = c(12,9), sd = c(2,3), n = c(25,30), tail = "right", graph = FALSE)
-  expect_equal(found$p_value[1], pt((12 - 9)/(sqrt( sd[1] ^ 2 / n[1] + sd[2] ^ 2 / n[2])),50.742007,lower.tail = FALSE))
+  found <- test(test = "t_test.two", null = 0, x_bar = c(12,9), sigma = c(2,3), n = c(25,30), tail = "right", graph = FALSE)
+  expect_equal(found$p_value[1], pt((12 - 9)/(sqrt( sigma[1] ^ 2 / n[1] + sigma[2] ^ 2 / n[2])),50.742007,lower.tail = FALSE))
 
   #testing for two tails
-  found <- test(test = "t_test.two", null = 0, x_bar = c(12,9), sd = c(2,3), n = c(25,30), tail = "two", graph = FALSE)
-  expect_equal(found$p_value[1], pt((12 - 9)/(sqrt( sd[1] ^ 2 / n[1] + sd[2] ^ 2 / n[2])),50.742007,lower.tail = TRUE) * 2)
+  found <- test(test = "t_test.two", null = 0, x_bar = c(12,9), sigma = c(2,3), n = c(25,30), tail = "two", graph = FALSE)
+  expect_equal(found$p_value[1], pt((12 - 9)/(sqrt( sigma[1] ^ 2 / n[1] + sigma[2] ^ 2 / n[2])),50.742007,lower.tail = TRUE) * 2)
 
 })
 
@@ -134,4 +134,25 @@ test_that("test preforms the chi-squared Independence test correclty", {
                             10.701,7.847,6.522,6.726,8.153,6.115,4.484)
                           ,nrow = 3, byrow = TRUE )
    expect_equal(round(found$expected_count[1:3,1:5],3), expect_table)
+})
+
+
+test_that("test preforms confidence intervals correctly",{
+  found <- test(test = "t_conf.one", x_bar = 12, sigma = 3, level = c(.95), n = 20)
+
+  expect_equal(found$lower_bound, (12 + -1 * qt(.975,19) * 3 / sqrt(20)))
+  expect_equal(found$upper_bound, (12 + 1 * qt(.975,19) * 3 / sqrt(20)))
+
+  found <- test(test = "t_conf.two", x_bar = c(12,10), sigma = c(3,2), level = .95, n = c(20,30))
+  df <- ((3 ^ 2 / 20 + 2 ^ 2 / 30) ^ 2) / sum((1 / (c(20,30) - 1)) * (c(3,2) ^ 2 / c(20,30)) ^ 2)
+  expect_equal(found$lower_bound, ((12 - 10) + -1 * qt(.975,df) * sqrt(9 / 20 + 4 / 30)))
+  expect_equal(found$upper_bound, ((12 - 10) + 1 * qt(.975,df) * sqrt(9 / 20 + 4 / 30)))
+
+  found <- test(test = "z_conf.one", p_hat = .4, level = .90, n = 20)
+  expect_equal(found$lower_bound, (.4 + -1 * qnorm(.95) * sqrt(.4 * .6 / 20)))
+  expect_equal(found$upper_bound, (.4 + 1 * qnorm(.95) * sqrt(.4 * .6 / 20)))
+
+  found <- test(test = "z_conf.two", p_hat = c(.4,.6), level = .99, n = c(20,30))
+  expect_equal(found$lower_bound, ((.4 - .6) + -1 * qnorm(.995) * sqrt((.4 * .6 / 20) + (.6 * .4 / 30))))
+  expect_equal(found$upper_bound, ((.4 - .6) + 1 * qnorm(.995) * sqrt((.4 * .6 / 20) + (.6 * .4 / 30))))
 })
